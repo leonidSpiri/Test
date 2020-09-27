6c27bd2934568056aa21e5e1e6c0e6464a726050
lenchik <2003.leonid2003@gmail.com>
2020-09-27 20:07:09 +0300

    Cool

---
+++ src/FOR/Task2.java
@@ -0,0 +1,13 @@
+package FOR;
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 30; i++) {
+            for (int j = 1; j < 30; j++) {
+                System.out.print("\t"+ i*j);
+            }
+            System.out.println();
+        }
+    }
+}
