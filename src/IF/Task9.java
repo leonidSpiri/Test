e8a23220c94c75cb95df559bb267762f4609e8a7
lenchik <2003.leonid2003@gmail.com>
2020-09-27 19:04:53 +0300

    Cool

---
+++ src/IF/Task9.java
@@ -0,0 +1,23 @@
+package IF;
+
+import java.util.Scanner;
+// не получается
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int y1 = in.nextInt();
+
+        int x2 = in.nextInt();
+        int y2 = in.nextInt();
+
+        if ( (x1*x2>0 && y1*y2>0) ||
+                (x1*x2<0 && y1*y2>0) ||
+                (x1*x2>0 && y1*y2<0) ||
+                (x1*x2<0 && y1*y2<0)
+        ){
+            System.out.println("Пересечения нет");
+        }
+
+    }
+}
