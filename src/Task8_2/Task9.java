b6c6c65e9221af0e1d83032575cd23324875ae71
lenchikspiridonchik <2003.leonid2003@gmail.com>
2021-04-01 17:22:36 +0300

    cool

--- src/Task8_2/Task9.java
+++ src/Task8_2/Task9.java
@@ -1,8 +1,5 @@
 package Task8_2;
 
-/*1)	Вася составляет 5-буквенные слова, в которых есть только буквы С, И, Р, О, П, причём в каждом слове обязательно есть ровно одна буква О,
- при этом стоять она может только после согласной. Каждая из других допустимых букв может встречаться в слове любое количество раз или не
-  встречаться совсем. Сколько существует таких слов, которые может написать Вася?*/
 public class Task9 {
     public static void main(String[] args) {
         String slovo = "СИРОП";
@@ -17,17 +14,17 @@ public class Task9 {
                                     String.valueOf(slovo.charAt(i3)) +
                                     String.valueOf(slovo.charAt(i4)) +
                                     String.valueOf(slovo.charAt(i5));
-                            int alreadyhas =0;
-                            for (int i = 1; i < buff.length(); i++) {
-                                if (buff.charAt(i) == 'О' ) {
-                                    if (buff.charAt(i - 1) == 'С' || buff.charAt(i - 1) == 'Р' || buff.charAt(i - 1) == 'П') {
-                                        if (buff.charAt(i - 1) != 'О' && alreadyhas <2) {
-                                            result++;
-                                            alreadyhas++;
-                                            System.out.println(buff + " " + buff.charAt(i-1));
-                                        }
+                            int o = 0;
+                            boolean istrue = true;
+                            if (buff.charAt(0) != 'О') {
+                                for (int i = 1; i < buff.length(); i++) {
+                                    if (buff.charAt(i) == 'О') {
+                                        o++;
+                                        if (!(buff.charAt(i - 1) == 'С' || buff.charAt(i - 1) == 'Р' || buff.charAt(i - 1) == 'П'))
+                                            istrue = false;
                                     }
                                 }
+                                if (o == 1 && istrue) result++;
                             }
                         }
                     }
