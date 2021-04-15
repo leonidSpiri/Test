d1f8f674bbc92c2fe47986a8e8f87cbb7932bda2
lenchikspiridonchik <2003.leonid2003@gmail.com>
2021-04-15 23:55:39 +0300

    cool

---
+++ src/OLIMP/Tin1.kt
@@ -0,0 +1,19 @@
+package OLIMP
+
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val one = scanner.nextInt()
+    val two = scanner.nextInt()
+    val three = scanner.nextInt()
+    val four = scanner.nextInt()
+    when {
+        one == three -> println("$two $one $three $four")
+        one == four -> println("$two $one $four $three")
+        two == three -> println("$one $two $three $four")
+        two == four -> println("$one $two $four $three")
+        else -> println("-1")
+    }
+}
+
---
+++ src/OLIMP/Tin2.kt
@@ -0,0 +1,19 @@
+package OLIMP
+
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val vxod = scanner.next()
+    var istrue = true
+    val glas = arrayOf("a", "e", "i", "o", "u", "y")
+    for (i in 0..vxod.length - 2) {
+        if (glas.contains(vxod[i].toString()) && glas.contains(vxod[i + 1].toString())
+                || !glas.contains(vxod[i].toString()) && !glas.contains(vxod[i + 1].toString())) {
+            istrue = false
+        }
+
+    }
+    if (istrue) println("YES")
+    else println("NO")
+}
