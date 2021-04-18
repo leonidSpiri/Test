04ac1cf52b11d5385a7276a4e8758a1856a01790
lenchikspiridonchik <2003.leonid2003@gmail.com>
2021-04-18 18:15:05 +0300

    cool

--- src/Task16/Polkov1.java
+++ src/Task16/Polkov1.java
@@ -1,5 +1,11 @@
 package Task16;
-
+/*Задание 16
+Алгоритм вычисления значения функции F(n), где n – натуральное число, задан следующими соотношениями:
+F(n) = n + 15, при n ≤ 5
+F(n) = F(n//2) + n*n*n - 1, при чётных n > 5
+F(n) = F(n-1) + 2*n*n + 1, при нечётных n > 5
+Здесь // обозначает деление нацело. Определите количество натуральных значений n из отрезка [1; 1000], для которых значение F(n) содержит не менее двух цифр 8.
+164*/
 public class Polkov1 {
     public static void main(String[] args) {
         int otvet = 0;
@@ -8,7 +14,7 @@ public class Polkov1 {
             int eight = 0;
             for (int i = 0; i < result.length(); i++) {
                 if (result.charAt(i)=='8') eight++;
-                if (eight == 2) otvet++;
+                if (eight >=2) otvet++;
             }
         }
         System.out.println(otvet);
---
+++ src/Task22/Polkov1.java
@@ -0,0 +1,30 @@
+package Task22;
+/* {
+  int x, L, M;
+  scanf("%d", &x);
+  L = x; M = 65;
+  if (L % 2 == 0) M = 52;
+  while (L != M) {
+    if(L > M)
+      L = L - M;
+    else
+      M = M - L;
+  }
+  printf("%d", M);
+}*/
+public class Polkov1 {
+    public static void main(String[] args) {
+        for (int v = 10000; v >= 100; v--) {
+            int x = v, L = 1, M =0;
+            L = x; M = 65;
+            if (L % 2 == 0) M = 52;
+            while (L != M) {
+                if(L > M)
+                    L = L - M;
+                else
+                    M = M - L;
+            }
+            if (M == 26) System.out.println(v); //83
+        }
+    }
+}
