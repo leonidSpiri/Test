36c21a00debf74ca0c19cf39feb0d372ff0245bb
lenchikspirdonchik <2003.leonid2003@gmail.com>
2021-04-29 13:22:48 +0300

    cool

--- src/Task15/ege17.java
+++ src/Task15/ege17.java
@@ -1,12 +1,12 @@
 package Task15;
 
-public class ege16 {
+public class ege17 {
     public static void main(String[] args) {
-        for (int a = 100; a >= 0; a--) {
+        for (int a = 100; a > 0; a--) {
             boolean flag = true;
             for (int x = 0; x < 10000; x++) {
                 for (int y = 0; y < 10000; y++) {
-                    boolean buff = ((x * x > a) || (x <= 7)) && ((y >= 4) || (y * y <= a));
+                    boolean buff = (((x&59)==0) || ( (x&a)!=0 || (x&51)!=0 ));
 
                     if (!buff) {
                         flag = false;
