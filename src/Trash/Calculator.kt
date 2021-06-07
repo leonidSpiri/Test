827a16366062248e421bef8185de4276f1d0760f
lenchikspiridonchik <2003.leonid2003@gmail.com>
2021-06-07 19:08:37 +0300

    cool

--- src/Trash/Calculator.kt
+++ src/Trash/Calculator.kt
@@ -8,23 +8,42 @@ fun main() {
     val stroka = "1 + 2 * ( 3 + 4 / 2 - ( 1 + 2 ) ) * 2 + 1"
     val chisloStack = ArrayDeque<Int>()
     val znakStack = ArrayDeque<Char>()
-    for (i in 0..stroka.lastIndex){
-        val vxod = stroka[i].toString()
-        if (lvl(vxod) == -1) chisloStack.add(vxod.toInt())
-
-    }
 
 }
 
-fun lvl(znak: String):Int{
-    return when (znak) {
-        "+" -> 1
-        "-" -> 1
-        "*" -> 2
-        "/" -> 2
-        "(" -> 0
-        ")" -> 0
-        " " -> 0
-        else -> -1
+class Parser {
+    private var operators = "+-*/"
+    private var delimiters = "() $operators"
+    var flag = true
+
+    private fun isDelimiter(enter: String): Boolean {
+        if (enter.length != 1) return false
+        for (i in 0..delimiters.length) {
+            if (enter[0] == delimiters[i]) return true
+        }
+        return false
+    }
+
+    private fun isOperator(enter: String): Boolean {
+        if (enter == "u-") return true
+        for (i in 0..operators.length) {
+            if (enter[0] == operators[i]) return true
+        }
+        return false
+    }
+
+    private fun priority(enter: String): Int {
+        if (enter == "(") return 1
+        if (enter == "+" || enter == "-") return 2
+        return if (enter == "*" || enter == "/") 3 else 4
+    }
+
+    fun chet(enter: String): List<String> {
+        val postfix = ArrayList<String>()
+        val stack = ArrayDeque<String>()
+
+
+
+        return postfix
     }
 }
